5f24322503cf7ac06695238abf6974b4028fff0b
Laputa <nguyencong160602@gmail.com>
2023-10-27 20:01:03 +0700

    update minimap

---
+++ Assets/Scripts/System/MinimapFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    public Transform player;  
+
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 newPosition = player.position;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+    }
+}
