02228c2597eb7168021e267a9bf422c36370a286
ThanhDq8902 <quangthanh08092002@gmail.com>
2023-11-07 03:27:39 +0700

    ThanhDQ/dialogFinal

---
+++ Assets/Scripts/DrawftScripts/DialogFinal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogFinal : MonoBehaviour
+{
+    public GameObject dialogPannel;
+    public TextMeshProUGUI message;
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Khi người chơi vào khoảng cách gần với vật thể, cho phép hiển thị đoạn hội thoại
+            dialogPannel.SetActive(true);  
+            if (PlayerPrefs.GetInt("QuantityItem")>1 && PlayerPrefs.GetInt("QuantityItem") < 150) {
+                message.text = $"So luong avat pham : {PlayerPrefs.GetInt("QuantityItem")}/150";
+
+            }
+            else
+            {
+                message.text = $"Vay la ban da nhan du vat pham roi !";
+                StartCoroutine(LoadTotalMapAfterDelay(3f));
+            }
+        }
+    }
+
+
+    IEnumerator LoadTotalMapAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1); 
+    }
+    protected void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogPannel.SetActive(false);
+        }
+    }
+
+}
